67acd757f907af2c7439ec4ad0d52d58a5a53920
M. Arif Mardhavi <arifmardhavi@gmail.com>
2023-04-15 13:34:48 +0700

    [DOCS] add script biodata

---
+++ biodata.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Biodata</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+  </head>
+  <body>
+    
+    <div class="container my-5 text-center">
+    	<div class="row">
+    		<div class="card p-2 col-md-12" >
+    			<center>
+			  		<img src="assets/fotoku.jpeg" class="card-img-top" alt="img-biodata" style="width:3cm; height: 4cm;">
+    			</center>
+			  <div class="card-body">
+			    <h5 class="card-title">M. ARIF MARDHAVI</h5>
+			    <div class="row py-3">
+				    <ul class="list-group list-group-flush">
+					  <li class="list-group-item">Banyuwangi, 11 Maret 20002</li>
+					  <li class="list-group-item">Laki - Laki</li>
+					  <li class="list-group-item">Jl. Semampir tengah IIA No.17 Sukolilo, Kota SBY.</li>
+					</ul>
+			    </div>
+			    <div class="row py-3">
+			    	<div class="card mb-2 p-1 text-white border-0 bg-primary">
+			    		<h3>RIWAYAT PENDIDIKAN</h3>
+			    	</div>
+					<ol class="list-group list-group-numbered">
+					  <li class="list-group-item d-flex justify-content-between align-items-start">
+					    <div class="ms-2 me-auto">
+					      <div class="fw-bold">MI SYAMSUL HUDA</div>
+					    </div>
+					    <span class="badge bg-primary rounded-pill">2007 - 2013</span>
+					  </li>
+					  <li class="list-group-item d-flex justify-content-between align-items-start">
+					    <div class="ms-2 me-auto">
+					      <div class="fw-bold">SMPN 2 KALIPURO</div>
+					    </div>
+					    <span class="badge bg-primary rounded-pill">2013 - 2017</span>
+					  </li>
+					  <li class="list-group-item d-flex justify-content-between align-items-start">
+					    <div class="ms-2 me-auto">
+					      <div class="fw-bold">SMKN 1 BANYUWANGI</div>
+					    </div>
+					    <span class="badge bg-primary rounded-pill">2017 - 2020</span>
+					  </li>
+					  <li class="list-group-item d-flex justify-content-between align-items-start">
+					    <div class="ms-2 me-auto">
+					      <div class="fw-bold">UPN VETERAN JAWA TIMUR</div>
+					    </div>
+					    <span class="badge bg-primary rounded-pill">2020 - 2024</span>
+					  </li>
+					</ol>
+			    </div>
+			    <div class="row py-3">
+			    	<div class="card mb-2 p-1 text-white border-0 bg-primary">
+			    		<h3>HOBI</h3>
+			    	</div>
+			    	<center>
+				    	<button type="button" class="btn btn-outline-primary col-md-2 mx-1">Makan</button>
+				    	<button type="button" class="btn btn-outline-danger col-md-2 mx-1">Rafting</button>
+				    	<button type="button" class="btn btn-outline-success col-md-2 mx-1">Camping</button>
+				    	<button type="button" class="btn btn-outline-warning col-md-2 mx-1">Nonton Film</button>
+				    	<button type="button" class="btn btn-outline-dark col-md-1 mx-1">Game</button>
+				    	<button type="button" class="btn btn-outline-info col-md-2 mx-1">Fishing</button>
+			    	</center>
+
+			    </div>
+			  </div>
+			</div>
+    	</div>
+    </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  </body>
+</html>
