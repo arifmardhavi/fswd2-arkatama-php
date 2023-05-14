2b67cb15309f06f059e40b37b4c661888d8ce073
M. Arif Mardhavi <arifmardhavi@gmail.com>
2023-05-14 11:48:43 +0700

    add user

---
+++ user/koneksi.php
@@ -0,0 +1,15 @@
+<?php
+$host = "localhost"; // Ganti dengan host database Anda
+$username = "root"; // Ganti dengan username database Anda
+$password = ""; // Ganti dengan password database Anda
+$dbname = "arkatama_store"; // Ganti dengan nama database Anda
+
+// Membuat koneksi
+$conn = new mysqli($host, $username, $password, $dbname);
+
+// Memeriksa koneksi
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
+?>
+
